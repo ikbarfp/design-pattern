23e7f521a722acb8ecfab88b9ce30788e24a1d57
ikbarfp <ikbar@finansia.com>
2023-08-27 21:55:54 +0700

    update execute code comments in main

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	// Call Start() function to know how this certain pattern will work
-	// with some debug log, so you will know exactly the process and the results
+	// Call Execute() function to know how this certain pattern will work
+	// with additional debug log, so you will know exactly the process and the results
 }
