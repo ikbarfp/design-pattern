909b9d6057ab0758e772e2670c37b36241b9da2e
ikbarfp <ikbar@finansia.com>
2023-09-06 18:39:49 +0700

    add new execution command

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import "github.com/ikbarfp/design-pattern/behavioural/strategy"
+
 func main() {
+
 	// Call Execute() function to know how this certain pattern will work
 	// with additional debug log, so you will know exactly the process and the results
+	strategy.Execute()
+
 }
