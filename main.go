0fb13923efcad6456fdc27681e2d40eb251d08a4
ikbarfp <ikbar@finansia.com>
2023-09-11 15:17:13 +0700

    execute factorymethod pattern

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/ikbarfp/design-pattern/behavioural/strategy"
+import (
+	"github.com/ikbarfp/design-pattern/creational/factorymethod"
+)
 
 func main() {
 
 	// Call Execute() function to know how this certain pattern will work
 	// with additional debug log, so you will know exactly the process and the results
-	strategy.Execute()
 
+	//strategy.Execute()
+
+	factorymethod.Execute()
 }
