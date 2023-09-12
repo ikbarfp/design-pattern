188b9432a6191d5e1d66a9b98de3ae108bd3e2c0
ikbarfp <ikbar@finansia.com>
2023-09-12 08:44:06 +0700

    update main

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
-import (
-	"github.com/ikbarfp/design-pattern/creational/factorymethod"
-)
+import "github.com/ikbarfp/design-pattern/creational/abstractfactory"
 
 func main() {
 
 	// Call Execute() function to know how this certain pattern will work
 	// with additional debug log, so you will know exactly the process and the results
-
+	/*
+		.
+		.
+		.
+	*/
 	//strategy.Execute()
 
-	factorymethod.Execute()
+	//factorymethod.Execute()
+
+	abstractfactory.Execute()
 }
