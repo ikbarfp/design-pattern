f1afb0b129b4f18a58b5da9511f7ed69d76a3b2a
ikbarfp <ikbar@finansia.com>
2023-09-15 08:16:22 +0700

    update main

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 
 func main() {
 
-	// Call Execute() function to know how this certain pattern will work
+	// Call {package_name}.Execute() function to know how this certain pattern will work
 	// with additional debug log, so you will know exactly the process and the results
 	/*
-		.
-		.
-		.
+		i.e :
+		- strategy.Execute()
+		- factorymethod.Execute()
+		- so on and so forth
 	*/
 
 	// strategy.Execute()
